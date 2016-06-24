8c7813dc99f0e95fb0e79fbdb8876d58b17222fa
Bernd Lörwald <bloerwald@googlemail.com>
2016-06-25 00:28:31 +0200

    add convenience .ds_store filter

--- normalize.sh
+++ normalize.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 tr -s '/\\' '/' < ~/Documents/Blizzard/listfile/listfile.txt \
     | tr '[:upper:]' '[:lower:]' | tr -d '\015' \
     | sort -u \
+    | grep -vi .ds_store \
 	   > ~/Documents/Blizzard/listfile/listfile.tmp
 
 mv ~/Documents/Blizzard/listfile/listfile.tmp \
