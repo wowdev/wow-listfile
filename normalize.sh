af11e20fb3d7e992915743ba40e36d57581630b9
Michał Kałużny <maku@justmaku.org>
2017-03-01 12:42:32 +0100

    Removed empty lines

--- normalize.sh
+++ normalize.sh
@@ -6,6 +6,7 @@ tr -s '/\\' '/' < listfile.txt \
     | tr '[:upper:]' '[:lower:]' | tr -d '\015' \
     | LC_ALL=C sort -u \
     | grep -vi .ds_store \
+    | grep -v '^$' \
 	   > listfile.tmp
 
 mv listfile.tmp \
