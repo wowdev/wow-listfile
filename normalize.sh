19f1587773e7d36cd6040fa912440d7e99e7a546
Bernd Lörwald <bloerwald@googlemail.com>
2018-02-14 23:47:38 +0100

    normalize: properly handle grep exit code

--- normalize.sh
+++ normalize.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 tr -s '/\\' '/' < listfile.txt \
     | tr '[:upper:]' '[:lower:]' | tr -d '\015' \
     | LC_ALL=C sort -u \
-    | grep -vi .ds_store \
-    | grep -v '^$' \
+    | (grep -vi .ds_store || true) \
+    | (grep -v '^$' || true) \
 	   > listfile.tmp
 
 mv listfile.tmp \
