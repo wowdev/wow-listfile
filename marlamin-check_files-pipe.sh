bd5f26942c7c104b29a8813899bd063503ea6b55
Bernd Lörwald <bloerwald@googlemail.com>
2018-02-14 23:45:44 +0100

    marlamin-check_files: be more resiliant and add some logging

--- marlamin-check_files-pipe.sh
+++ marlamin-check_files-pipe.sh
@@ -4,11 +4,18 @@ _scriptdir=$($(which greadlink readlink 2>/dev/null | head -n 1) -f ${BASH_SOURC
 scriptdir="${_scriptdir%/*}"
 
 tmp_f=$(mktemp $PWD/check_files_XXXXXX)
+trap 'rm -f ${tmp_f}*' EXIT
 
-cat > ${tmp_f}
+sort -u > "${tmp_f}"
+if ! test -s "${tmp_f}"
+then
+  echo "- no files to check" >&2
+  exit 0
+fi
+echo "- checking $(head -n1 "${tmp_f}") and $(($(cat "${tmp_f}" | wc -l)-1)) others..." >&2
 
-split -a 5 -l 1499 ${tmp_f} ${tmp_f}-part
+split -a 5 -l 1000 "${tmp_f}" "${tmp_f}-part"
 
-"${scriptdir}/marlamin-check_files.sh" ${tmp_f}-part*
+"${scriptdir}/marlamin-check_files.sh" "${tmp_f}-part"*
 
-rm -f ${tmp_f}*
+echo "- done" >&2
--- marlamin-check_files.sh
+++ marlamin-check_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -uo pipefail
+set -euo pipefail
 
 for file in ${@}
 do
-  wc -l $file
-  curl -s https://bnet.marlam.in/checkFiles.php --data files="$(cat ${file})" | sed -e 's,<br>,%,g' | tr '%' '\n' | grep ^Added
+  echo "-- checking $(head -n1 "${file}") and $(($(cat "${file}" | wc -l)-1)) others..." >&2
+  curl --silent --show-error "https://bnet.marlam.in/checkFiles.php" --data files="$(cat "${file}")" | sed -e 's,<br>,%,g' | tr '%' '\n' | (grep ^Added || true) | sed -e "s,^Added,+,"
 done
