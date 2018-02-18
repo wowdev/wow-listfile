5dee994ee93cb4fd25557b1cdaec146f4d06c09b
Bernd Lörwald <bloerwald@googlemail.com>
2018-02-18 12:20:18 +0100

    add-additionals: more blp suffixes, replace suffixes, only noliquid minimaps

--- add-blp-additionals.sh
+++ add-blp-additionals.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 file="${1}"
 
-#suffixes="$(echo _{n,e,s,hd,h}.blp)"
-suffixes="_n.blp _e.blp _s.blp _hd.blp _h.blp"
+#suffixes="$(echo _{n,e,s,hd,h,l,u,m,f}.blp)"
+suffixes="_n.blp _e.blp _s.blp _hd.blp _h.blp _l.blp _u.blp _m.blp _f.blp"
 
 tmp_f=$(mktemp $PWD/check_files_XXXXXX)
 trap 'rm -f "${tmp_f}"' EXIT
@@ -14,9 +14,10 @@ cat "${file}" \
   | (grep blp$ || true) \
   > "${tmp_f}"
 
-sed -e 's,\(.*/\),\1noliquid_,' "${tmp_f}"
+grep "world/minimaps/" "${tmp_f}" | sed -e 's,\(.*/\),\1noliquid_,'
 
 for suff in ${suffixes}
 do
   sed -e "s,.blp$,${suff}," "${tmp_f}"
+  grep '_[a-z].blp$' "${tmp_f}" | sed -e "s,_[a-z].blp$,${suff},"
 done
