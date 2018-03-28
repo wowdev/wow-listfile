cd7bb964c0c55fc22cf92b032085a7e1398a5d60
Bernd Lörwald <bloerwald@googlemail.com>
2018-03-28 22:06:58 +0200

    add-wdt-additionals.sh: add mpv

--- add-wdt-additionals.sh
+++ add-wdt-additionals.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 file="${1}"
 
-#suffixes="$(echo _{occ,lgt,fogs}.wdt .wdl)"
-suffixes="_occ.wdt _lgt.wdt _fogs.wdt .wdl"
+#suffixes="$(echo _{occ,lgt,fogs,mpv}.wdt .wdl)"
+suffixes="_occ.wdt _lgt.wdt _fogs.wdt _mpv.wdt .wdl"
 
 tmp_f=$(mktemp $PWD/check_files_XXXXXX)
 trap 'rm -f "${tmp_f}"' EXIT
