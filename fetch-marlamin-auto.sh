7202a8e85e6e8663a8decbe07e69c57ea7673993
Martin Benjamins <marlamin@marlamin.com>
2019-02-09 00:17:51 +0100

    Create fetch-marlamin-auto.sh

---
+++ fetch-marlamin-auto.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+_scriptdir=$($(which greadlink readlink 2>/dev/null | head -n 1) -f ${BASH_SOURCE})
+scriptdir="${_scriptdir%/*}"
+
+curl --compressed "https://bnet.marlam.in/listfile.php" >> listfile.txt
+"${scriptdir}/normalize.sh"
+
+if ${1:-false}
+then
+  git add listfile.txt
+  if [ -n "${3-}" ]
+  then
+    git commit -m "bnet.marlam.in listfile $(LANG=C date)" -m "$3"
+  else
+    git commit -m "bnet.marlam.in listfile $(LANG=C date)"
+  fi
+  if ${2:-true}
+  then
+    git push
+  fi
+fi
